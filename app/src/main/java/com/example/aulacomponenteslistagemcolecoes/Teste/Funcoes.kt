e4d82f924561bdf873c353245e24441f25a7d8db
Ryan Silva <ryansilva15.dev.7@gmail.com>
2025-05-01 19:43:53 -0300

    Componentes de Listagem

---
+++ app/src/main/java/com/example/aulacomponenteslistagemcolecoes/Teste/Funcoes.kt
@@ -0,0 +1,15 @@
+package com.example.aulacomponenteslistagemcolecoes.Teste
+
+
+class matematica{
+    fun somar(n1: Int,n2: Int ) : Int{//Método
+        return n1 + n2
+    }
+}
+fun somar(n1: Int,n2: Int ) : Int{//Função
+    return n1 + n2
+fun main(){
+    /*val matematica = matematica()
+    val retorno = matematica.somar(10,10)*/
+    println(retorno)
+}
---
+++ app/src/main/java/com/example/aulacomponenteslistagemcolecoes/Teste/Recursos.java
@@ -0,0 +1,8 @@
+package com.example.aulacomponenteslistagemcolecoes.Teste;
+
+public class Recursos {
+
+    public static final class id {
+        public static final int text_numero =  1009;
+    }
+}
